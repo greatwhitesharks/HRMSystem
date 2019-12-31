884d96a564e225ac0e4daed1f2db6410eed8fd35
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2019-12-31 08:01:05 +0530

    add findjob method

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,4 +1,5 @@
 const JobTitleRepository =require('../repositories/jobTitle.repository');
+const PayGradeRepository =require('../repositories/payGrade.repository');
 const EmployeeAccount = require('../models/employeeRecord.model'); //please check this one
 
 
@@ -10,13 +11,20 @@ class AdminService {
   constructor(db) {
     this.db = db;
   }
+  async findJob(job){
+    const jobRepo = new JobTitleRepository(this.db);
+    await jobrepo.find({
+      title:job
+    });
+    return true // need to return a boolean value from the result
+  }
 
   async addJobTitle(job,salary) {
 
     const jobRepo = new JobTitleRepository(this.db);
 
     await jobrepo.create({
-      job,
+      title:job,
       salary
     });
 
@@ -25,12 +33,45 @@ class AdminService {
         salary
     );
   }
+  
   async removeJobTitle(job) {
 
     const jobRepo = new JobTitleRepository(this.db);
 
     await jobrepo.delete({
-      job
+      title:job
+    });
+  }
+
+  async findPayGrade(job){
+    const paygradeRepo = new PayGradeRepository(this.db);
+    await paygraderepo.find({
+      title:job
+    });
+    return true // need to return a boolean value from the result
+  }
+
+  async addPayGrade(job,salary) {
+
+    const jobRepo = new PayGradeRepository(this.db);
+
+    await jobrepo.create({
+      title:job,
+      salary
+    });
+
+    return new PayGrade(
+        job,
+        salary
+    );
+  }
+  
+  async removePayGrade(job) {
+
+    const jobRepo = new PayGradeRepository(this.db);
+
+    await jobrepo.delete({
+      title:job
     });
   }
 }
