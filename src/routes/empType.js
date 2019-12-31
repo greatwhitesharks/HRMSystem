5a19037e477dd2e46d1c57c1b688de5f58b86a1a
Vihanga Jayawickrama <vihanga.dewmini.97@gmail.com>
2019-12-31 19:00:30 +0530

    minor bug fixes

--- src/routes/empType.js
+++ src/routes/empType.js
@@ -1,7 +1,7 @@
-const EmpTypeController = require('./controllers/.empType.controller');
+const EmpTypeController = require('./controllers/empType.controller');
 const express = require('express');
 const router = express.Router();
 
-router.post('/create', EmpTypeController.create);
+router.post('/empType', EmpTypeController.create);
 
 module.exports = router;
